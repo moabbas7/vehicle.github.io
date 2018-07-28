06454e94cdee60d78216a3c81d9925615e27579e
moabbas7 <mirmohammed14@gmail.com>
2018-07-28 17:22:46 +0530

    vehicle_interface.py

--- vehicle_interface.py
+++ vehicle_interface.py
@@ -1,5 +1,15 @@
 from car import car
+from truck import truck
 
 
 obj= car("ac","steering_wheel","seat_belt","audio_play")
 print(obj.deploy_airbags())
+
+obj1 = truck(100,60,6,40,1500,10,"blue")
+print(obj1.load())
+print(obj1.unload())
+print(obj1.no_of_wheels)
+print(obj1.speed)
+print(obj1.weight)
+print(obj1.mileage)
+print(obj1.color)
